9f59372748e00d835aa3de3111aff128c3b92dda
rghvawsmcp <rghvaws@gmail.com>
2025-11-18 18:39:11 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,12 @@ plugins {
     alias(libs.plugins.android).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.ksp).apply(false)
+    id("org.sonarqube") version "7.0.1.6134"
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "rghvawsmcp_simplecalculator")
+    property("sonar.organization", "rghvawsmcp")
+  }
 }
